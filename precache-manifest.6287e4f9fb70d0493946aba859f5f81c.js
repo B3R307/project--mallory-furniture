a9b5f85c038686d448499ed6c6180632507e67cb
B3R307 <talzolteotlmeztli@gmail.com>
2019-02-07 16:15:44 -0600

    Updates

---
+++ precache-manifest.6287e4f9fb70d0493946aba859f5f81c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf133ddb743cc80134c10b1567e40a6e",
+    "url": "/project--mallory-furniture/static/media/category-desks.bf133ddb.png"
+  },
+  {
+    "revision": "4acfcd218a874af9b336",
+    "url": "/project--mallory-furniture/static/css/main.51e68c06.chunk.css"
+  },
+  {
+    "revision": "81912b802cee72a24178",
+    "url": "/project--mallory-furniture/static/js/1.81912b80.chunk.js"
+  },
+  {
+    "revision": "0d310f52ad6babea2058",
+    "url": "/project--mallory-furniture/static/js/runtime~main.0d310f52.js"
+  },
+  {
+    "revision": "29205eda9c80a44948a7d3a2b7fc50a3",
+    "url": "/project--mallory-furniture/static/media/mf-logo-white.29205eda.svg"
+  },
+  {
+    "revision": "436b30fae282e681dda437c291b7237d",
+    "url": "/project--mallory-furniture/static/media/mf-logo-black.436b30fa.svg"
+  },
+  {
+    "revision": "d4af0c6a37cfede57d4cd6bf3c06b43e",
+    "url": "/project--mallory-furniture/static/media/category-bedroom.d4af0c6a.png"
+  },
+  {
+    "revision": "4acfcd218a874af9b336",
+    "url": "/project--mallory-furniture/static/js/main.4acfcd21.chunk.js"
+  },
+  {
+    "revision": "763b9dc454bdedb9520e083091439ebf",
+    "url": "/project--mallory-furniture/static/media/category-miscellaneous.763b9dc4.png"
+  },
+  {
+    "revision": "d7be874aba647460c1fd3d606529c3d3",
+    "url": "/project--mallory-furniture/static/media/category-seating.d7be874a.png"
+  },
+  {
+    "revision": "39dbed6ee13ce415ec7f007009dd560d",
+    "url": "/project--mallory-furniture/static/media/category-storage.39dbed6e.png"
+  },
+  {
+    "revision": "1e58e90e280965480331d4f37dad7312",
+    "url": "/project--mallory-furniture/static/media/category-tables.1e58e90e.png"
+  },
+  {
+    "revision": "8db12e274d73cc961935f37962ade9b0",
+    "url": "/project--mallory-furniture/static/media/landing-splash-bg.8db12e27.png"
+  },
+  {
+    "revision": "5fdc42e6e2b0da732ac4fd0fe08be6d0",
+    "url": "/project--mallory-furniture/index.html"
+  }
+];
